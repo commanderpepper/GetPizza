f7854a6ae38b0bbb141eddee22b6caed01b3c9d0
commanderpepper <ahmadhumza7@gmail.com>
2019-05-14 23:29:30 -0400

    Added in stuff related to Google Maps

--- app/src/main/java/commanderpepper/getpizza/map/MapView.kt
+++ app/src/main/java/commanderpepper/getpizza/map/MapView.kt
@@ -5,11 +5,13 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.SupportMapFragment
+import commanderpepper.getpizza.R
 
-class MapView() : Fragment() {
+class MapView : SupportMapFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         println("hi")
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragement_map, container, false)
     }
 }
