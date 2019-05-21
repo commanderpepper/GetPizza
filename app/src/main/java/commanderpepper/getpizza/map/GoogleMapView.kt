cb7bdaf7645a971f024ba25c20b1461ae366be69
commanderpepper <ahmadhumza7@gmail.com>
2019-05-20 22:33:35 -0400

    Moved super.onCreate to first line of onCreate, fixed unresolved XML issue

--- app/src/main/java/commanderpepper/getpizza/map/GoogleMapView.kt
+++ app/src/main/java/commanderpepper/getpizza/map/GoogleMapView.kt
@@ -8,20 +8,12 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 
-class GoogleMapView : SupportMapFragment(), OnMapReadyCallback {
+class GoogleMapView : SupportMapFragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(commanderpepper.getpizza.R.layout.google_map_view, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        
-    }
-
-    override fun onMapReady(p0: GoogleMap?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 
 }
--- app/src/main/java/commanderpepper/getpizza/map/MapActivity.kt
+++ app/src/main/java/commanderpepper/getpizza/map/MapActivity.kt
@@ -2,15 +2,34 @@ package commanderpepper.getpizza.map
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import commanderpepper.getpizza.R
 
-class MapActivity : AppCompatActivity() {
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        val sydney = LatLng(-33.852, 151.211)
+        googleMap.addMarker(
+            MarkerOptions().position(sydney)
+                .title("Marker in Sydney")
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
