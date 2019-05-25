c14f6879c2cdfca08f86c3da4e492606568a4456
commanderpepper <ahmadhumza7@gmail.com>
2019-05-24 22:44:14 -0400

    Moved the map view into its own fragment, separate from the activity.

--- app/src/main/java/commanderpepper/getpizza/map/GoogleMapView.kt
+++ app/src/main/java/commanderpepper/getpizza/map/GoogleMapView.kt
@@ -1,19 +1,72 @@
 package commanderpepper.getpizza.map
 
+
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+import com.google.android.gms.maps.MapsInitializer
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 
-class GoogleMapView : SupportMapFragment() {
+import commanderpepper.getpizza.R
+
+class GoogleMapView : Fragment() {
+    private lateinit var googleMap: GoogleMap
+    private lateinit var mapView : MapView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+
+        val rootView = inflater.inflate(R.layout.fragment_google_map_view, container, false)
+
+        mapView = rootView.findViewById(R.id.pmap) as MapView
+        mapView.onCreate(savedInstanceState)
+
+        mapView.onResume()
+
+        try {
+            MapsInitializer.initialize(activity!!.applicationContext)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        mapView.getMapAsync { mMap ->
+            googleMap = mMap
+
+            // For dropping a marker at a point on the Map
+            val sydney = LatLng(-34.0, 151.0)
+            googleMap.addMarker(MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"))
+
+            // For zooming automatically to the location of the marker
+            val cameraPosition = CameraPosition.Builder().target(sydney).zoom(5f).build()
+            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+        }
 
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(commanderpepper.getpizza.R.layout.google_map_view, container, false)
+        return rootView
     }
 
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
 
+    override fun onPause() {
+        super.onPause()
+        mapView.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
 }
--- app/src/main/java/commanderpepper/getpizza/map/MapActivity.kt
+++ app/src/main/java/commanderpepper/getpizza/map/MapActivity.kt
@@ -2,34 +2,21 @@ package commanderpepper.getpizza.map
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+
 import commanderpepper.getpizza.R
 
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        val pizzaMap = GoogleMapView()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.map_container, pizzaMap)
+        fragmentTransaction.commit()
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        // Add a marker in Sydney, Australia,
-        // and move the map's camera to the same location.
-        val sydney = LatLng(-33.852, 151.211)
-        googleMap.addMarker(
-            MarkerOptions().position(sydney)
-                .title("Marker in Sydney")
-        )
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
 }
