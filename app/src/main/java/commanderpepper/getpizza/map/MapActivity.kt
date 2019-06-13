f74239a502b9e05d7461fa1edeeb9bfd6ae64e64
commanderpepepr <ahmadhumza7@gmail.com>
2019-06-12 23:10:31 -0400

    Added LocationManager

--- app/src/main/java/commanderpepper/getpizza/map/MapActivity.kt
+++ app/src/main/java/commanderpepper/getpizza/map/MapActivity.kt
@@ -1,5 +1,6 @@
 package commanderpepper.getpizza.map
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +10,10 @@ import commanderpepper.getpizza.retrofit.ZomatoService
 import io.reactivex.disposables.Disposable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import android.location.LocationManager
+import android.content.Context.LOCATION_SERVICE
+
+
 
 
 class MapActivity : AppCompatActivity() {
@@ -28,17 +33,10 @@ class MapActivity : AppCompatActivity() {
         fragmentTransaction.add(R.id.map_container, pizzaMap)
         fragmentTransaction.commit()
 
-//        performSearch()
-    }
-
-    private fun performSearch() {
-        val latlon = mutableListOf<Pair<Double, Double>>()
-        disposable = zomatoService.performSearch(40.76, -73.5)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-//            .map { searchResults -> searchResults.restaurants.map { it.restaurant.location.longitude to it.restaurant.location.latitude } }
-            .subscribe { result -> Log.d("Humza", result.toString()) }
-        disposable!!.dispose()
+        val lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        val longitude = location.longitude
+        val latitude = location.latitude
     }
 
 }
